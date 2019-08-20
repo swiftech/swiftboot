5eeb53a124f1771ed73cab333a74588ccc66db45
allen <allen@humanpool.com.cn>
2019-08-20 13:59:08 +0800

    密码工具类(加盐)

--- swiftboot-utils/src/main/java/org/swiftboot/util/PasswordUtils.java
+++ swiftboot-utils/src/main/java/org/swiftboot/util/PasswordUtils.java
@@ -11,23 +11,51 @@ public class PasswordUtils {
 
     /**
      * 创建密码
+     *
      * @param original
      * @return
      */
-    public String createPassword(String original) {
+    public static String createPassword(String original) {
         return CryptoUtils.md5(original);
     }
 
     /**
+     * 加盐 MD5 密码(16进制表示）
+     *
+     * @param src
+     * @param salt
+     * @return
+     */
+    public static String md5salt(String src, String salt) {
+        return CryptoUtils.md5salt(src, salt);
+    }
+
+    /**
      * 验证密码
+     *
      * @param toBeVerified
      * @param encrypted
      * @return
      */
-    public boolean verifyPassword(String toBeVerified, String encrypted) {
+    public static boolean verifyPassword(String toBeVerified, String encrypted) {
         if (StringUtils.isBlank(toBeVerified)) {
             return false;
         }
         return Objects.equals(CryptoUtils.md5(toBeVerified), encrypted);
+    }
+
+    /**
+     * 验证密码
+     *
+     * @param toBeVerified
+     * @param encrypted
+     * @param salt
+     * @return
+     */
+    public static boolean verifyPassword(String toBeVerified, String encrypted, String salt) {
+        if (StringUtils.isBlank(toBeVerified)) {
+            return false;
+        }
+        return Objects.equals(CryptoUtils.md5salt(toBeVerified, salt), encrypted);
     }
 }
