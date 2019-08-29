592fd53408437de6c0ea39eb8a385fc8ff6a9c01
allen <allenny@163.com>
2019-08-30 02:04:04 +0800

    ``

---
+++ swiftboot-web/src/main/java/org/swiftboot/web/util/HttpServletCookieUtils.java
@@ -0,0 +1,21 @@
+package org.swiftboot.web.util;
+
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author switech 2019-08-29
+ *
+ **/
+public class HttpServletCookieUtils {
+
+    public static String getCookieValue(HttpServletRequest request, String name) {
+        Cookie cookieToken = WebUtils.getCookie(request, name);
+        if (cookieToken == null) {
+            return null;
+        }
+        return cookieToken.getValue();
+    }
+}
