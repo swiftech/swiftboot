4be76a1e361adab0f54bd2aeb940f5612b32129a
allen <allen@humanpool.com.cn>
2019-09-17 16:10:19 +0800

    demo and doc update

---
+++ swiftboot-demo/src/main/java/org/swiftboot/demo/filter/SwiftbootDemoFilter.java
@@ -0,0 +1,21 @@
+package org.swiftboot.demo.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SwiftbootDemoFilter extends OncePerRequestFilter {
+    private Logger log = LoggerFactory.getLogger(SwiftbootDemoFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("执行 SwiftbootDemoFilter");
+        filterChain.doFilter(request, response);
+    }
+}
