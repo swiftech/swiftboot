83904be3d2205b43216592a6baef99d2088eabe6
allen <awsksupport@gmail.com>
2020-02-05 20:55:26 +0800

    change demo to using swiftboot-auth for app user

---
+++ swiftboot-demo/src/main/java/org/swiftboot/demo/model/dao/AppUserCustomizeDao.java
@@ -0,0 +1,9 @@
+package org.swiftboot.demo.model.dao;
+
+/**
+ * App用户数据查询接口
+ *
+ * @author swiftech 2020-01-06
+ **/
+public interface AppUserCustomizeDao {
+}
---
+++ swiftboot-demo/src/main/java/org/swiftboot/demo/model/dao/impl/AppUserCustomizeDaoImpl.java
@@ -0,0 +1,12 @@
+package org.swiftboot.demo.model.dao.impl;
+
+import org.swiftboot.demo.model.dao.AppUserCustomizeDao;
+import org.swiftboot.web.model.dao.impl.BaseCustomizeDaoImpl;
+
+/**
+ * App用户数据查询接口实现
+ *
+ * @author swiftech 2020-01-06
+ **/
+public class AppUserCustomizeDaoImpl extends BaseCustomizeDaoImpl implements AppUserCustomizeDao {
+}
