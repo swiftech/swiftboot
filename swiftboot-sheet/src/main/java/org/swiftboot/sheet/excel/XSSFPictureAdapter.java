6a18d1c65476fdf0b5670da759e18ca086246c84
allen <awsksupport@gmail.com>
2023-08-11 12:04:30 +0800

    fix: exception when read any shapes other than pictures in ms excel file; v2.4.2

--- swiftboot-sheet/src/main/java/org/swiftboot/sheet/excel/XSSFPictureAdapter.java
+++ swiftboot-sheet/src/main/java/org/swiftboot/sheet/excel/XSSFPictureAdapter.java
@@ -6,6 +6,8 @@ import org.apache.poi.xssf.usermodel.XSSFClientAnchor;
 import org.apache.poi.xssf.usermodel.XSSFPicture;
 import org.apache.poi.xssf.usermodel.XSSFShape;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.swiftboot.sheet.meta.Position;
 
 import java.util.HashMap;
@@ -17,6 +19,8 @@ import java.util.Map;
  */
 public class XSSFPictureAdapter implements PictureAdapter {
 
+    private static final Logger log = LoggerFactory.getLogger(XSSFPictureAdapter.class);
+
     @Override
     public Map<Position, Picture> getPictures(Sheet sheet) {
         Map<Position, Picture> ret = new HashMap<>();
@@ -24,8 +28,14 @@ public class XSSFPictureAdapter implements PictureAdapter {
         List<XSSFShape> shapes = xssfSheet.getDrawingPatriarch().getShapes();
         for (XSSFShape shape : shapes) {
             XSSFClientAnchor anchor = (XSSFClientAnchor) shape.getAnchor();
-            XSSFPicture p = (XSSFPicture) shape;
-            ret.put(new Position(anchor.getRow1(), (int) anchor.getCol1()), p);
+            log.debug(String.format("Try to read picture from row %d col %d", anchor.getRow1(), anchor.getCol1()));
+            if (shape instanceof XSSFPicture) {
+                XSSFPicture p = (XSSFPicture) shape;
+                ret.put(new Position(anchor.getRow1(), (int) anchor.getCol1()), p);
+            }
+            else {
+                log.debug(String.format("This is not a picture object but '%s'", shape.getClass().getSimpleName()));
+            }
         }
         return ret;
     }
