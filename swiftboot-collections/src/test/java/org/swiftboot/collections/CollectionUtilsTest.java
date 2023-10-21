8ebfa549ced562226843493158ac56f726910f0d
swiftech <awsksupport@gmail.com>
2023-10-21 19:16:29 +0800

    remove deprecated classes

--- swiftboot-collections/src/test/java/org/swiftboot/collections/CollectionUtilsTest.java
+++ swiftboot-collections/src/test/java/org/swiftboot/collections/CollectionUtilsTest.java
@@ -62,40 +62,6 @@ public class CollectionUtilsTest {
     }
 
     @Test
-    public void testClassify() {
-        List<TestBean> list = new ArrayList<TestBean>() {
-            {
-                add(new TestBean("a", "a1"));
-                add(new TestBean("a", "a2"));
-                add(new TestBean("a", "a3"));
-                add(new TestBean("b", "b1"));
-                add(new TestBean("b", "b2"));
-                add(new TestBean("b", "b3"));
-            }
-        };
-        Map<Object, Collection> classify
-                = CollectionUtils.classify(list, (ClassifyFilter<TestBean>) TestBean::getKey);
-
-        for (Object k : classify.keySet()) {
-            System.out.println(k);
-            System.out.println(classify.get(k));
-        }
-        Collection<TestBean> ca = classify.get("a");
-        Assertions.assertNotNull(ca);
-        Assertions.assertEquals(3, ca.size());
-        for (TestBean e : ca) {
-            Assertions.assertEquals("a", e.getValue().substring(0, 1));
-        }
-
-        Collection<TestBean> cb = classify.get("b");
-        Assertions.assertNotNull(cb);
-        Assertions.assertEquals(3, cb.size());
-        for (TestBean e : cb) {
-            Assertions.assertEquals("b", e.getValue().substring(0, 1));
-        }
-    }
-
-    @Test
     public void testSortListCollection() {
         List<TestBean> list = new ArrayList<TestBean>() {
             {
@@ -149,7 +115,7 @@ public class CollectionUtilsTest {
         }
     }
 
-    public static class TestBean {// implements Comparable<TestBean> {
+    public static class TestBean {
         String key;
         String value;
 
