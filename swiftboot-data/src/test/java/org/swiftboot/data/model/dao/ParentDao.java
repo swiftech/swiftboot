ed5359a313b067512ee83f07e3a8d7ed8d9ca4a3
swiftech <awsksupport@gmail.com>
2025-02-06 00:16:38 +0800

    version 3.0.0; update dependencies; fix docs

--- swiftboot-data/src/test/java/org/swiftboot/data/model/dao/ParentDao.java
+++ swiftboot-data/src/test/java/org/swiftboot/data/model/dao/ParentDao.java
@@ -1,6 +1,7 @@
 package org.swiftboot.data.model.dao;
 
 import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import org.swiftboot.data.model.entity.ParentEntity;
@@ -12,7 +13,7 @@ import java.util.Optional;
  * @author swiftech
  **/
 @Repository
-public interface ParentDao extends PagingAndSortingRepository<ParentEntity, String> {
+public interface ParentDao extends PagingAndSortingRepository<ParentEntity, String>, CrudRepository<ParentEntity, String> {
 
     @QueryHints(value = { @QueryHint(name = org.hibernate.annotations.QueryHints.FLUSH_MODE, value = "COMMIT") })
     Optional<ParentEntity> findByName(String name);
