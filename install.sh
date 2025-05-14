1ab5a593b3968ba1153f5b18f4e7ed2350025855
swiftech <awsksupport@gmail.com>
2025-05-15 00:05:10 +0800

    fix: install test-jar doesn't work

--- install.sh
+++ install.sh
@@ -11,6 +11,6 @@ mvn -N install
 mvn clean install -Dmaven.test.skip=true -f swiftboot-utils/pom.xml
 mvn clean install -Dmaven.test.skip=true -f swiftboot-collections/pom.xml
 mvn clean install -Dmaven.test.skip=true -f swiftboot-data/pom.xml
-mvn jar:test-jar -f swiftboot-data/pom.xml
+mvn test-compile jar:test-jar -f swiftboot-data/pom.xml
 mvn install:install-file -Dfile=target/swiftboot-data-${version}-tests.jar -DgroupId=com.github.swiftech -DartifactId=swiftboot-data -Dclassifier=tests -Dpackaging=test-jar -f swiftboot-data/pom.xml
 mvn clean install -Dmaven.test.skip=true
