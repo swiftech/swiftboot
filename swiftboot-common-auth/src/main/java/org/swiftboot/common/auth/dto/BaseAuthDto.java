7e76dc95b9755c53b46328b2075fbdc15a67effe
swiftech <awsksupport@gmail.com>
2025-05-22 11:41:08 +0800

    [common-auth] update docs

--- swiftboot-common-auth/src/main/java/org/swiftboot/common/auth/dto/BaseAuthDto.java
+++ swiftboot-common-auth/src/main/java/org/swiftboot/common/auth/dto/BaseAuthDto.java
@@ -11,11 +11,11 @@ public abstract class BaseAuthDto {
     /**
      * optional if using Cookie under Session mode
      */
-    @Schema(description = "Access Token", example = "772eb2add9b64e40972468c779b3b952")
+    @Schema(description = "Access Token", example = "6d567f2dfe72946ef5f45a61d799348b6d281c1d66bf5cad83aef50964bbd84dcc4c51f7adc638fd17eec68b300c73a783475fd3eeb35c88c566760993ffd906d43cb3f9a7126f7fbd66a56da345209c7816bb9de2f0ae57201394c21e45b01bebb27ec7abcdc65a374fe6cf35ae37c49c5017e5e04f57ae8d9e8620bac30cf5")
     @JsonProperty(value = "access_token")
     private String accessToken;
 
-    @Schema(description = "Access token expires at time in milliseconds")
+    @Schema(description = "Access token expires at time in milliseconds", example = "1747885080")
     @JsonProperty(value = "expires_at")
     private Long expiresAt;
 
--- swiftboot-common-auth/src/main/java/org/swiftboot/common/auth/dto/BaseRefreshTokenDto.java
+++ swiftboot-common-auth/src/main/java/org/swiftboot/common/auth/dto/BaseRefreshTokenDto.java
@@ -10,11 +10,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
  */
 public abstract class BaseRefreshTokenDto extends BaseAuthDto {
 
-    @Schema(description = "Refresh Token, only for JWT mode")
+    @Schema(description = "Refresh Token, only for JWT mode", example = "5a81b20361e335bdfa30ee0b35b4a0f8dc921bda72b1dccf0866a694ad0b58fc76996816d9c8f9069dd1d77e0323420ed3ee4a59893c74df5e9701d130d81694882d0d84d454928ac8638b036dd8785891d59baf0fb9afed2fbcedee158d02ce69d67e447a999492ca99172987184d0e42aaae7451920431ade3a1814ad6090b")
     @JsonProperty(value = "refresh_token")
     private String refreshToken;
 
-    @Schema(description = "Refresh token expires at time in milliseconds, only for JWT mode")
+    @Schema(description = "Refresh token expires at time in milliseconds, only works for JWT mode", example = "1747885080")
     @JsonProperty(value = "refresh_token_expires_at")
     private Long refreshTokenExpiresAt;
 
