ff9faa360941be3b85648b7aee668aa7d466361c
swiftech <awsksupport@gmail.com>
2026-03-17 21:13:14 +0800

    update examples

--- swiftboot-common-auth/src/test/java/org/swiftboot/common/auth/JwtTokenProviderTest.java
+++ swiftboot-common-auth/src/test/java/org/swiftboot/common/auth/JwtTokenProviderTest.java
@@ -13,7 +13,7 @@ import org.swiftboot.util.IdUtils;
  */
 public class JwtTokenProviderTest {
 
-    private static final String SECRET = "d9c0fd4d4aa9c7c228cd8194efcff1d0ec200310ba41e106a198d05025cea15f147f2bfe6d5a415656bf53c4b843d269ccf7921d65fc652f00008eda95393e709ec0d4c12b6f1fd18da0d470ca9750313c082d49f869311029663deb7ae7864791d22ab3424b8a85f6c63f73b86c49d6d50d1b21527c9274e4f1ff2143fe5b28bf69d14688b15d4711f0c03c25db2624ee830a3450693ad5e5070d4a7a1fc046ffe1cd1e9fa0b32f58b041e2e859bfdf92eff72a9f1ebd387470429dbba6af57b30b0ba76f68e53ccb87949972649c34322811f1b3966ee87ffa9dc38d6a2737de88f0a9aaa704d52b25d1f3ab7048c5e55e140cdc71aae0b35644ac7834c19f";
+    private static final String SECRET = "1888dfcab0d44dc2eeb0311e9dc79f05f14ed90a508e5c53003a0f3f568c4430";
 
     private static JwtTokenProvider jwtTokenProvider;
 
@@ -43,7 +43,8 @@ public class JwtTokenProviderTest {
     public void testGetUsername() {
         String uid = IdUtils.makeID("uid");
         AccessToken accessToken = jwtTokenProvider.generateAccessToken(uid, "swiftboot");
-        System.out.println(accessToken);
+        System.out.println(accessToken.getTokenValue());
+        System.out.println(accessToken.getExpiresAt());
         Assertions.assertEquals(uid, jwtTokenProvider.getUserId(accessToken.getTokenValue()));
         Assertions.assertEquals("swiftboot", jwtTokenProvider.getAddition(accessToken.getTokenValue(), JwtTokenProvider.USERNAME_KEY));
         Assertions.assertEquals("swiftboot", jwtTokenProvider.getUsername(accessToken.getTokenValue()));
